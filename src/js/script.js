505047c9a65b9e2b1545a91f24120b465a611630
Patrick Teulings <patrickteulings@gmail.com>
2018-12-09 22:41:55 +0100

    Social share
    Added social share module
    @todo - add Linked in

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 
 import ElementToggle from './modules/toggle/ElementToggle';
 import Searchbox from './elements/EmptySearchbox';
+import SocialShare from './elements/SocialShare';
 import * as WebFont from 'webfontloader'
 
 /**
@@ -37,4 +38,16 @@ const searchboxes = document.querySelectorAll('[data-module="searchbox"]');
 
 for (let searchbox of searchboxes) {
   searchbox = new Searchbox(searchbox);
+}
+
+/**
+  *
+  * @desc Social Share buttons
+  *
+  */
+
+const shareIcons = document.querySelectorAll('[data-module="socialshare"]');
+
+for (let shareIcon of shareIcons) {
+  shareIcon = new SocialShare(shareIcon);
 }
